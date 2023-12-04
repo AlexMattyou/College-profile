db210b2ecb2c62193bf95c2ab89c9632c00afaa4
Alex Matthew <98243103+AlexMattyou@users.noreply.github.com>
2023-12-05 04:18:17 +0530

    Update 5)a-coin-change.py

--- semester-3/DAA-lab/5)a-coin-change.py
+++ semester-3/DAA-lab/5)a-coin-change.py
@@ -1,10 +1,17 @@
 def coinChange(coins, amount):
     dp = [0] + [float('inf')] * amount
+    comb = [[]] + [[] for _ in range(amount)]
+    
     for coin in coins:
         for i in range(coin, amount + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[-1] if dp[-1] != float('inf') else -1
+            if dp[i - coin] + 1 < dp[i]:
+                dp[i] = dp[i - coin] + 1
+                comb[i] = comb[i - coin] + [coin]
+                
+    return dp[-1] if dp[-1] != float('inf') else -1, comb[-1]
 
 coins = [1, 2, 3]
 amount = 10
-print(coinChange(coins, amount))
+minCoins, coinComb = coinChange(coins, amount)
+print(f"Minimum coins needed: {minCoins}")
+print(f"Coin combination: {coinComb}")
